4239a47d4889b04b7ed6f8de59ed752662a7d168
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2019-09-21 14:46:57 +0900

    add comments

--- ioutil/main.go
+++ ioutil/main.go
@@ -7,14 +7,16 @@ import (
 
 type Page struct {
 	Title string
-	Body []byte
+	Body  []byte
 }
 
+// 上記のPage構造体をもつ関数
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
+	// filenameで定義したファイル名で0600(所有者のみ読み書き可能)にしてファイル作成
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
-func loadPage(title string)(*Page, error){
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -24,10 +26,10 @@ func loadPage(title string)(*Page, error){
 }
 
 func main() {
-	p1 := &Page{Title: "test", Body: []byte("This is a test..")}
+	p1 := &Page{Title: "test", Body: []byte("This is a sample test..")}
 	p1.save()
 
 	p2, _ := loadPage(p1.Title)
 	fmt.Println(string(p2.Body))
-	
+
 }
